9423a9b4823fdcb69b3de8fa2a891b549c5c276a
LohithaYalavarthi <lohithayalavarthi95@gmail.com>
2018-12-04 04:39:13 +0530

    Updates

---
+++ precache-manifest.07ec90515f441a2ce9985a08a882a295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c826f60f3df4f947edda",
+    "url": "/LohithaYalavarthi/static/js/runtime~main.c826f60f.js"
+  },
+  {
+    "revision": "ff62c1af7a685ea5cd54",
+    "url": "/LohithaYalavarthi/static/js/main.ff62c1af.chunk.js"
+  },
+  {
+    "revision": "b2ba0f303a324b1ed494",
+    "url": "/LohithaYalavarthi/static/js/1.b2ba0f30.chunk.js"
+  },
+  {
+    "revision": "ff62c1af7a685ea5cd54",
+    "url": "/LohithaYalavarthi/static/css/main.de57c16f.chunk.css"
+  },
+  {
+    "revision": "5892424f106fc6a97aab50013a37e0ef",
+    "url": "/LohithaYalavarthi/index.html"
+  }
+];
